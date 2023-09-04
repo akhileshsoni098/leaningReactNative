85a5d268aca8aab1ccb2b2801740d01d6e858455
Akhilesh Soni <akhileshsoni098@gmail.com>
2023-09-04 14:07:26 +0530

    jjj

--- reactLearningFirstProject/App.js
+++ reactLearningFirstProject/App.js
@@ -1,45 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { View, Text, Button } from 'react-native'
 
-import CompanyData from "./components/CompanyData";
-
-
 const App = () => {
 
+const [name , setName] = useState("Akhilesh");
 
+ function testName(){
+setName("Siddhu")
+ }
   return (
     <View>
-<CompanyData/>
-      <Text style={{fontSize:30}}> Components </Text>
-
-<UserData/>
+<Text style ={{fontSize: 30}}> {name} </Text>
+<Button title="Update Name" onPress={testName}/>
     </View>
   )
 
-
 }
-
-
-const UserData = ()=>{
-
-  return (
-    <View>
-    
-    <Text style={{fontSize:20}}> Name: Akhilesh Soni </Text>
-
-    <Text style={{fontSize:20}}> Age: 24 </Text>
-
-    <Text style={{fontSize:20}}> Email: akhilesh9988@gmail.com </Text>
-
-    <Text style={{fontSize:20}}>  </Text>
-
-    </View>
-  )
-}
-
-
-
 
 
 export default App
